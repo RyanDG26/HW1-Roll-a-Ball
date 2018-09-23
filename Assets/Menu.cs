b1dac72bebde38408300197497d3cfdc82c663d4
Marc Ryan De Guia <degui003@cougars.csusm.edu>
2018-09-22 23:55:05 -0700

    First Commit

---
+++ Assets/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+    public Button PlayGame;
+    public Button QuitGame;
+	// Use this for initialization
+	void Start () {
+        PlayGame.onClick.AddListener(() => { SceneManager.LoadScene("Scene1"); });
+        QuitGame.onClick.AddListener(() => { Application.Quit(); });
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Menu2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu2 : MonoBehaviour
+{
+
+    public Button Back;
+   
+    // Use this for initialization
+    void Start()
+    {
+        Back.onClick.AddListener(() => { SceneManager.LoadScene("Menu Scene"); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
